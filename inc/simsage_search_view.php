7917ba335f52eb7eb8830d5b31b0fef4496d4f19
Rock de Vocht <peter@peter.co.nz>
2020-09-30 12:46:45 +0100

    Added send disabled

--- inc/simsage_search_view.php
+++ inc/simsage_search_view.php
@@ -340,7 +340,7 @@
 
     <!-- ***************** -->
     <!-- NO Search results -->
-    <tr class="no-search-results">
+    <tr class="no-search-results" style="display: none;">
         <td>
             <div class="no-results-found-box">
                 <span class="no-results-found">No Results found for</span>
