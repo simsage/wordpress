e945be3dccf905a233fda40fba24bdbde3c71ed0
Rock de Vocht <rock@simsage.nz>
2020-10-30 10:56:11 +0000

    fixed get plan

--- simsage_admin.php
+++ simsage_admin.php
@@ -569,7 +569,7 @@ class simsage_admin
             $archive_file = fopen($filename, "wb");
 
             $plugin_options = get_option(SIMSAGE_PLUGIN_NAME);
-            $num_docs = int($plan["numDocs"]);
+            $num_docs = $plan["numDocs"];
             $num_qas = $plan["numQA"];
             if ($archive_file) {
                 debug_log("starting " . $filename);
